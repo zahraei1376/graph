9f8a7e05cd4f78f64f9377d4217ad6f366f3138d
zahra <zahra0a1418@gmail.com>
2023-08-31 20:28:10 +0330

    add GraphWithAdjacencyList2 By using set and starting the node names from 0

--- component/GraphWithAdjacencyList.js
+++ component/GraphWithAdjacencyList.js
@@ -1,4 +1,4 @@
-class GraphWithAdjacencyList {
+export class GraphWithAdjacencyList {
     constructor() {
         this.adjacencyLists = [];
     }
@@ -9,14 +9,43 @@ class GraphWithAdjacencyList {
 
     addEdgeToAdjacency = (node1, node2, directed = false) => {
         if (!this.adjacencyLists[node1]) this.addAdjacency(node1);
-        else if (!this.adjacencyLists[node2]) this.addAdjacency(node2);
+        if (!this.adjacencyLists[node2]) this.addAdjacency(node2);
 
-        if (directed) {
-            this.adjacencyLists[node1].push(node2);
-        } else {
-            this.adjacencyLists[node1].push(node2);
+        this.adjacencyLists[node1].push(node2);
+        if (!directed) {
             this.adjacencyLists[node2].push(node1);
         }
+    }
 
+    print = () => {
+        let result = ``;
+        this.adjacencyLists.forEach((edges, key) => {
+            result += `${key} --> ${edges.join(",", " ")}` + "\n";
+        });
+        return result;
+    }
+}
+
+export class GraphWithAdjacencyList2 {
+    constructor(numberOfNodes) {
+        this.adjacencyLists = [];
+        for (let index = 0; index < numberOfNodes; index++) {
+            this.adjacencyLists[index] = new Set([]);
+        }
+    }
+
+    addEdgeToAdjacency = (node1, node2, directed = false) => {
+        this.adjacencyLists[node1].add(node2);
+        if (!directed) {
+            this.adjacencyLists[node2].add(node1);
+        }
+    }
+
+    print = () => {
+        let result = ``;
+        this.adjacencyLists.forEach((edges, key) => {
+            result += `${key} --> ${Array.from(edges).join(",", " ")}` + "\n";
+        });
+        return result;
     }
 }
---
+++ index.js
@@ -0,0 +1,47 @@
+import { GraphWithAdjacencyList, GraphWithAdjacencyList2 } from "./component/GraphWithAdjacencyList";
+const directed = false;
+const nodes = [0, 1, 2, 3, 4];
+const edges = [
+    {
+        node1: 0,
+        node2: 1,
+    },
+    {
+        node1: 0,
+        node2: 2,
+    },
+    {
+        node1: 0,
+        node2: 3,
+    },
+    {
+        node1: 0,
+        node2: 4,
+    },
+    {
+        node1: 3,
+        node2: 4,
+
+    },
+    {
+        node1: 2,
+        node2: 3,
+
+    },
+    {
+        node1: 1,
+        node2: 2,
+    },
+];
+
+const graphWithAdjacencyList = new GraphWithAdjacencyList2(nodes.length);
+
+edges.forEach(edge => {
+    graphWithAdjacencyList.addEdgeToAdjacency(edge.node1, edge.node2, directed);
+});
+
+console.log(graphWithAdjacencyList.print());
+
+
+
+
