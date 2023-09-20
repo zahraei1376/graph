f616ad7621651681e5a6969f3adb73b6e69df3cc
zahra <zahra0a1418@gmail.com>
2023-09-20 14:44:06 +0330

    add addEdge function in WeightedGrph class

--- GraphWithAdjacencyList/WeightedGraph.js
+++ GraphWithAdjacencyList/WeightedGraph.js
@@ -1,5 +1,16 @@
 export class WeightedGraph {
-    constructor() {
-        super();
+    constructor(vertices) {
+        this.vertices = vertices;
+        this.adjacencyLists = new Map();
+        for (const vertex of vertices) {
+            this.adjacencyLists.set(vertex, []);
+        }
+    }
+
+    addEdge = (node1, node2, weight, directed = false) => {
+        this.adjacencyLists.get(node1).push({ node: node2, weight });
+        if (directed) {
+            this.adjacencyLists.get(node2).push({ node: node2, weight });
+        }
     }
 };
